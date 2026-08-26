3c66f928b4f364d4b70b966f442ea0efe94d36ca
anshsingh1032 <thansh1032@gmail.com>
2026-08-27 01:19:02 +0530

    fix: fix minor bug

--- backend/nodemailer/nodemailer.config.js
+++ backend/nodemailer/nodemailer.config.js
@@ -1,17 +1,27 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
+  family: 4,
+
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+
+  tls: {
+    minVersion: "TLSv1.2",
+  },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 export const sender = {
   email: process.env.SMTP_USER,
-  name: "PRAGYAN - The Coding Club of CSE",
+  name: "SIH Team",
 };
